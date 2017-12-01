7b04e7be19551e4b81a644ea18f12fb237495b4f
Aleš Menzel <alesmenzel@gmail.com>
2017-12-01 17:07:03 +0100

    init

---
+++ app.js
@@ -0,0 +1,340 @@
+var debug = require('debug')('draw-my-thing:server');
+
+let tmp = new Date();
+setInterval(() => {
+  let ms = new Date() - tmp - 1000;
+  if (ms >= 100 ) {
+    console.log('Event loop blocked for ' + ms + ' ms');
+  }
+  tmp = new Date();
+}, 1000);
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
+
+var index = require('./routes/index');
+
+var app = express();
+var http = require('http').Server(app);
+var io = socketio(http);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('*', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+// ******************** INITIAL STATE ******************** //
+const words = [
+  'vanilla',
+  'ice cream',
+  'jelly'
+];
+let players = {
+  byId: {},
+  allIds: []
+};
+let word = '';
+let currentPlayer = 0;
+let gameRunning = false;
+
+const state = {
+  roudTime: 20, //s
+  timer: null,
+  answered: []
+}
+
+
+
+io.on('connection', (socket) => {
+  const playerId = socket.id;
+
+  // ******************** DISCONNECT ******************** //
+  socket.on('disconnect', disconnect);
+
+  function disconnect() {
+    const player = players.byId[playerId];
+
+    if (!player) {
+      return;
+    }
+
+    removePlayer(playerId);
+    socket.broadcast.emit('player-disconnected', player.id);
+
+    if (players.allIds.length === 0) {
+      gameRunning = false;
+    }
+
+    console.log('User ' + player.name + ' disconnected');
+    console.log('Number of players: ' + players.allIds.length);
+  }
+
+
+  // ******************** REGISTER / START GAME ******************** //
+  socket.on('register', connect);
+
+  function connect(name) {
+    const player = {
+      id: socket.id,
+      name,
+      points: 0
+    };
+
+    addPlayer(player);
+    socket.broadcast.emit('player-connected', players.byId[socket.id]);
+    socket.emit('init-state', { players, ...getGuessingWord() });
+
+    startGame();
+
+    console.log('User ' + name + ' connected');
+    console.log('Number of players: ' + players.allIds.length);
+  }
+
+  function addPlayer(player) {
+    players = {
+      allIds: [...players.allIds, player.id],
+      byId: {
+        ...players.byId,
+        [player.id]: player
+      },
+    }
+  }
+
+  function removePlayer(id) {
+    const index = players.allIds.indexOf(id);
+
+    if (index === -1) {
+      return;
+    }
+
+    const {[id]: _noop, ...rest} = players.byId;
+
+    players = {
+      allIds: [...players.allIds.slice(0, index), ...players.allIds.slice(index + 1)],
+      byId: { ...rest }
+    }
+  }
+
+
+
+  // ******************** GAME ******************** //
+  function pickNextPlayer() {
+    let nextIndex = 0;
+    if (currentPlayer) {
+      nextIndex = players.allIds.indexOf(currentPlayer) + 1;
+    }
+
+    if (nextIndex >= players.allIds.length) {
+      nextIndex = 0;
+    }
+
+    currentPlayer = players.allIds[nextIndex];
+  }
+
+  function pickNextWord() {
+    word = words[Math.floor(Math.random() * words.length)];
+  }
+
+  function startGame() {
+    if (gameRunning) {
+      return;
+    }
+    gameRunning = true;
+
+    nextRound();
+  }
+
+  function getGuessingWord() {
+    return {
+      word: word.replace(/[^\s]/g, '_'),
+      activePlayer: currentPlayer,
+      timeLeft: state.timeLeft
+    };
+  }
+
+  function nextRound() {
+    clearInterval(state.timer);
+    state.timeLeft = state.roudTime;
+    state.answered = [];
+
+    if (players.allIds.length === 0) {
+      return;
+    }
+
+    pickNextPlayer();
+    pickNextWord();
+
+    debug(`New round, word: ${word}, player: ${players.byId[currentPlayer].name}`);
+
+    const newWord = getGuessingWord();
+
+    const guessedWord = {
+      word,
+      timeLeft: state.timeLeft
+    }
+
+    // Send word mask to everyone else
+    io.emit('new-word', newWord);
+    // Send the word to current player
+    io.to(currentPlayer).emit('word', guessedWord);
+    // Clear canvas
+    io.emit('clear-canvas');
+
+    state.timer = setInterval(() => {
+      if (state.timeLeft === 0) {
+        nextRound();
+      }
+
+      updateTimer();
+    }, 1000);
+  }
+
+  function updateTimer() {
+    state.timeLeft = state.timeLeft - 1;
+    io.emit('timer', state.timeLeft);
+  }
+
+
+
+  // ******************** DRAWING ******************** //
+  socket.on('draw', (payload) => {
+    socket.broadcast.emit('draw', payload);
+  });
+
+  socket.on('clear-canvas', () => {
+    socket.broadcast.emit('clear-canvas');
+  });
+
+  socket.on('chat-message', (message) => {
+    debug('Chat message', message, word);
+
+    // Mask the aswer if the message contains it
+    if (message.toLowerCase().includes(word.toLowerCase())) {
+      message = message.replace(word, '*'.repeat(word.length));
+
+      if (playerId !== currentPlayer && !state.answered.includes(playerId)) {
+        wordGuessed();
+      }
+    }
+
+    socket.broadcast.emit('chat-message', {
+      id: socket.id,
+      message
+    });
+  });
+
+  function wordGuessed() {
+    state.answered = [...state.answered, playerId];
+    players = {
+      ...players,
+      byId: {
+        ...players.byId,
+        [playerId]: {
+          ...players.byId[playerId],
+          points: players.byId[playerId].points + state.timeLeft
+        }
+      }
+    }
+
+    io.emit('points', {
+      id: playerId,
+      points: players.byId[playerId].points
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+http.listen(port);
+http.on('error', onError);
+http.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = http.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
---
+++ public/javascripts/main.js
@@ -0,0 +1,328 @@
+// ******************** CONSTANTS ******************** //
+const KEY_ENTER = 13;
+
+// ******************** DOM ******************** //
+const canvas = document.getElementById('canvas');
+const clearButton = document.getElementById('clear-canvas');
+const playersBox = document.getElementById('players');
+const chatWindow = document.getElementById('chat-window');
+const chatInput = document.getElementById('chat-input');
+const chatSubmit = document.getElementById('chat-submit');
+const wordBox = document.getElementById('word');
+const timerBox = document.getElementById('timer');
+
+const context = canvas.getContext("2d");
+const bufferCanvas = document.createElement('canvas');
+const bufferContext = bufferCanvas.getContext('2d');
+
+
+
+// ******************** LOGIN ******************** //
+const name = prompt('Please choose a name');
+socket.emit('register', name);
+
+
+
+// ******************** INITIAL STATE ******************** //
+let state = {
+  isDrawing: false,
+  canDraw: false,
+  color: 'black',
+  prevX: 0,
+  prevY: 0,
+  width: 2,
+  players: {
+    allIds: [],
+    byId: {}
+  },
+  activePlayer: null,
+  word: '',
+  timeLeft: 0
+};
+
+
+
+// ******************** CANVAS RESIZE ******************** //
+window.addEventListener('resize', resize, false);
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  context.drawImage(bufferCanvas, 0, 0);
+}
+
+resize();
+
+// Buffer canvas - stores canvas during resize
+bufferCanvas.width = canvas.width;
+bufferCanvas.height = canvas.height;
+
+
+
+// ******************** CHAT ******************** //
+chatSubmit.addEventListener('click', sendMessage, false);
+window.addEventListener('keydown', keyDown, false);
+socket.on('chat-message', receiveMessage);
+
+function keyDown(e) {
+  chatInput.focus();
+
+  if (e.keyCode === KEY_ENTER) {
+    sendMessage();
+  }
+}
+
+function sendMessage() {
+  const message = chatInput.value;
+  appendMessage(state.players.byId[socket.id].name, message);
+  chatInput.value = "";
+
+  socket.emit('chat-message', message);
+}
+
+function receiveMessage({ id, message }) {
+  appendMessage(state.players.byId[id].name, message);
+}
+
+function appendMessage(playername, msg) {
+  const message = document.createElement('div');
+  message.className = 'chat__message';
+  message.innerHTML = `<b>${playername}</b>: ${msg}`;
+
+  chatWindow.appendChild(message);
+}
+
+
+
+// ******************** DRAWING ******************** //
+const throttledMouseMove = _.throttle(mouseMove, 10);
+
+canvas.addEventListener('mousedown', mouseDown, false);
+canvas.addEventListener('mouseup', mouseUp, false);
+canvas.addEventListener('mouseout', mouseOut, false);
+canvas.addEventListener('mousemove', throttledMouseMove, false);
+socket.on('draw', draw);
+
+function mouseUp() {
+  state.isDrawing = false;
+}
+
+function mouseOut() {
+  state.isDrawing = false;
+}
+
+function mouseDown(e) {
+  if (!state.canDraw) {
+    return;
+  }
+
+  const { offsetX: x, offsetY: y } = e;
+  state.isDrawing = true;
+
+  const line = {
+    line: {
+      prevX: x,
+      prevY: y,
+      currX: x + 1,
+      currY: y + 1
+    },
+    color: state.color,
+    width: state.width
+  };
+
+  state.prevX = x;
+  state.prevY = y;
+
+  draw(line);
+  socket.emit('draw', line);
+}
+
+function mouseMove(e) {
+  if (!state.canDraw || !state.isDrawing) {
+    return;
+  }
+
+  const { offsetX: x, offsetY: y } = e;
+
+  const line = {
+    line: {
+      prevX: state.prevX,
+      prevY: state.prevY,
+      currX: x,
+      currY: y
+    },
+    color: state.color,
+    width: state.width
+  };
+
+  state.prevX = x;
+  state.prevY = y;
+
+  draw(line);
+  socket.emit('draw', line);
+}
+
+function draw({ line, color, width }) {
+  const { prevX, prevY, currX, currY } = line;
+
+  context.beginPath();
+  context.moveTo(prevX, prevY);
+  context.lineTo(currX, currY);
+  context.strokeStyle = color;
+  context.lineWidth = width;
+  context.stroke();
+  context.closePath();
+
+  bufferContext.drawImage(canvas, 0, 0);
+}
+
+
+
+// ******************** CLEAR CANVAS ******************** //
+socket.on('clear-canvas', clearCanvas);
+clearButton.addEventListener('click', clear, false);
+
+function clearCanvas() {
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  bufferContext.drawImage(canvas, 0, 0);
+}
+
+function clear() {
+  clearCanvas();
+  socket.emit('clear-canvas', true);
+}
+
+
+
+// ******************** SCORE BOARD ******************** //
+socket.on('init-state', initState);
+socket.on('player-connected', playerConnected);
+socket.on('player-disconnected', playerDisconnected);
+
+function initState(newState) {
+  console.log(newState);
+  state = {
+    ...state,
+    ...newState
+  }
+  updatePlayers();
+  updateWord();
+  updateTimer();
+}
+
+function playerConnected(player) {
+  addPlayer(player);
+  updatePlayers();
+}
+
+function playerDisconnected(id) {
+  removePlayer(id);
+  updatePlayers();
+}
+
+function addPlayer(player) {
+  state.players = {
+    allIds: [...state.players.allIds, player.id],
+    byId: {
+      ...state.players.byId,
+      [player.id]: player
+    }
+  }
+}
+
+function removePlayer(id) {
+  const index = state.players.allIds.indexOf(id);
+  if (index === -1) {
+    return;
+  }
+
+  const {[id]: _noop, ...rest} = state.players.byId;
+
+  state.players = {
+    allIds: [...state.players.allIds.slice(0, index), ...state.players.allIds.slice(index + 1)],
+    byId: { ...rest }
+  }
+}
+
+function updatePlayers() {
+  const markup = state.players.allIds.map((id, index) => {
+    const player = state.players.byId[id];
+    const points = player.points;
+
+    let icon = '';
+    let active = '';
+    if (id === state.activePlayer) {
+      active = 'score__player--active';
+      icon = '<i class="fa fa-pencil score__player-pencil"></i>';
+    }
+
+    let leader = '';
+    if (index === 0) {
+      active = 'score__player--leader';
+    }
+
+    return `
+      <div class="score__player ${active} ${leader}">
+        ${icon}
+        <span class="score__player-points">${points}</span>
+        ${player.name} [${player.id}]
+      </div>
+    `
+  });
+
+  playersBox.innerHTML = markup.join('');
+}
+
+
+
+// ******************** WORD ******************** //
+socket.on('new-word', newWord);
+socket.on('word', drawWord);
+socket.on('timer', timer);
+socket.on('points', points);
+
+function updateWord() {
+  wordBox.innerHTML = state.word;
+}
+
+function updateTimer() {
+  timerBox.innerHTML = state.timeLeft;
+
+  if (state.timeLeft < 15) {
+    timerBox.classList.add('round__timer--red');
+  } else {
+    timerBox.classList.remove('round__timer--red');
+  }
+}
+
+function newWord({ word, activePlayer, timeLeft }) {
+  state.activePlayer = activePlayer;
+  state.word = word;
+  state.canDraw = false;
+  state.timeLeft = timeLeft;
+  updatePlayers();
+  updateWord();
+  updateTimer();
+}
+
+function drawWord({ word, timeLeft }) {
+  state.activePlayer = socket.id;
+  state.word = word;
+  state.canDraw = true;
+  state.timeLeft = timeLeft;
+  updatePlayers();
+  updateWord();
+  updateTimer();
+}
+
+function timer(timeLeft) {
+  state.timeLeft = timeLeft;
+  updateTimer();
+}
+
+function points({ id, points }) {
+  state.players.byId[id].points = points;
+  updatePlayers();
+}
